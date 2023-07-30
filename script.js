35d7b7b5b2faf283c0f2d00c46068d53ef80b9be
Sam Cassidy <sam@cassidys.email>
2023-07-30 20:50:20 +0100

    Add basic game module with an array of remaining moves

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const game = (() => {
+  let remainingMoves;
+
+  const reset = function() {
+    remainingMoves =
+      [...Array(15).keys()].map((i) => i + 1);
+  }
+
+  const getRemainingMoves = () => console.log(remainingMoves);
+
+  return { reset, getRemainingMoves };
+})();
+
+game.reset();
+game.getRemainingMoves();
